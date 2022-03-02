83149ff29e852254973e1bb0e2065d8bdb2fade9
trunghuong9920 <trunghuongcdbk@gmail.com>
2022-03-02 21:34:19 +0700

    upload file

---
+++ LocAnh/main.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <math.h>
+using namespace std;
+using namespace cv;
+
+const int Max = 50;
+int a[Max][Max], K[Max][Max], b[Max][Max];
+int n, m;
+
+void nhapmatran() {
+	cout << "Nhap ma tran anh: ";
+	cout << "\nSo hang: ";
+	cin >> m;
+	cout << "So cot: ";
+	cin >> n;
+	for (int i = 0; i <= n+1; i++) {
+		for (int j = 0; j <= m+1; j++) {
+			a[i][j] = 0;
+		}
+	}
+	cout << "Nhap ma tran anh: \n";
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 1; j < m + 1; j++) {
+			cout << "a[" << i << "][" << j << "] = "; cin >> a[i][j];
+		}
+	}
+	cout << "Nhap ma tran mat na: \n";
+	for (int i = -1; i < 2; i++) {
+		for (int j = -1; j < 2; j++) {
+			cout << "K[" << i << "][" << j << "] = "; cin >> K[i][j];
+		}
+	}
+	cout << "\nMa tran anh la: \n";
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 1; j < m + 1; j++) {
+			cout <<a[i][j]<<"\t";
+		}
+		cout << endl;
+	}
+	cout << "\nMa tran mat na:\n";
+	for (int i = -1; i < 2; i++) {
+		for (int j = -1; j < 2; j++) {
+			cout << K[i][j] << "\t";
+		}
+		cout << endl;
+	}
+
+}
+
+void tuongquan_1(Mat img) {
+	cout << "Nhan Chap:\n";
+
+	int tong = 0;
+	for (int x = 1; x <= 8; x++) {
+		for (int y = 1; y < 8; y++) {
+			for (int i = -1; i < 2; i++) {
+				for (int j = -1; j < 2; j++) {
+					tong = tong + img.at<uchar>(x + i, y + j);
+				}
+			}
+			cout << tong << "\t";
+			tong = 0;
+		}
+		cout << "\n";
+	}
+}
+
+void tuongquan_2() {
+	int tong = 0;
+	cout << "\nMa tran phep tuong quan: \n";
+	for (int x = 1; x < n + 1; x++) {
+		for (int y = 1; y < m + 1; y++) {
+			for (int i = -1; i < 2; i++) {
+				for (int j = -1; j < 2; j++) {
+					tong = tong + a[x + i][y + j]*K[i][j];
+				}
+			}
+			cout << tong << "\t";
+			tong = 0;
+		}
+		cout << endl;
+	}
+
+	cout << "\nChua hóa Ma tran phep tuong quan: \n";
+	float temp1 = 0;
+	float temp2 = 0;
+	float chuanhoa = 0;
+	float max = 0;
+
+	for (int x = 1; x < n + 1; x++) {
+		for (int y = 1; y < m + 1; y++) {
+			for (int i = -1; i < 2; i++) {
+				for (int j = -1; j < 2; j++) {
+					tong = tong + a[x + i][y + j] * K[i][j];
+					temp1 = temp1 + pow(K[i][j], 2);
+					temp2 = temp2 + pow(a[x + i][y + j], 2);
+				}
+			}
+			chuanhoa = (tong * 1.0) / (sqrt(temp1) * sqrt(temp2));
+			cout << fixed<<chuanhoa << "\t";
+			if (max < chuanhoa) {
+				max = chuanhoa;
+			}
+			tong = 0;
+			chuanhoa = 0;
+			temp1 = 0;
+			temp2 = 0;
+		}
+		cout << endl;
+	}
+	cout << "Vung anh khop voi mau nhat: " << max;
+
+}
+
+int main() {
+	Mat img = imread("D://Documents//XLA//TH//Loc_anh//new.jpg", 1);
+	//imshow("Show IMG", img);
+	/*
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			cout << (int)img.at<uchar>(i, j) << " ";
+		}
+		cout << endl;
+	}
+	tuongquan_1(img);
+	*/
+	nhapmatran();
+	tuongquan_2();
+	waitKey();
+	return 0;
+}
---
+++ Xulyanh/VD1.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <math.h>
+using namespace std;
+using namespace cv;
+
+int a[50][50], n, m;
+
+void tangsang(Mat img, int c) {
+	for (int i = 0; i < img.rows; i++) {
+		for (int j = 0; j < img.cols; j++) {
+
+			for (int k = 0; k < 3; k++) {
+				img.at<Vec3b>(i, j)[k] = img.at<Vec3b>(i, j)[k] + c > 255 ? 255 : img.at<Vec3b>(i, j)[k] + c;
+
+				img.at<Vec3b>(i, j)[k] = img.at<Vec3b>(i, j)[k] + c < 0 ? 0 : img.at<Vec3b>(i, j)[k] + c;
+			}
+		}
+	}
+
+	imshow("Tang sang", img);
+
+}
+
+void tuongphan(Mat img, int contrast, int c) {
+	for (int i = 0; i < img.rows; i++) {
+		for (int j = 0; j < img.cols; j++) {
+
+			for (int k = 0; k < 3; k++) {
+				img.at<Vec3b>(i, j)[k] = saturate_cast<uchar>(contrast * (img.at<Vec3b>(i, j)[k]) + c);
+			}
+		}
+	}
+
+	imshow("Tuong phan", img);
+	
+}
+void amban(Mat img) {
+	for (int i = 0; i < img.rows; i++) {
+		for (int j = 0; j < img.cols; j++) {
+			img.at<uchar>(i, j) = 255 - img.at<uchar>(i, j);
+		}
+	}
+}
+
+void dosang(Mat img) {
+	float sum = 0;
+	for (int i = 0; i < img.rows; i++) {
+		for (int j = 0; j < img.cols; j++) {
+			for (int k = 0; k < 3; k++) {
+				sum += img.at<Vec3b>(i, j)[k] / 255;
+			}
+		}
+	}
+	float tb = (sum / img.rows * img.cols);
+	cout << "\nDo sang= " << sum;
+}
+void dotuongphan(Mat img) {
+	float sum = 0;
+	float sum2 = 0;
+	for (int i = 0; i < img.rows; i++) {
+		for (int j = 0; j < img.cols; j++) {
+			for (int k = 0; k < 3; k++) {
+				sum += pow((img.at<Vec3b>(i, j)[k] / 255),2);
+				sum2 += img.at<Vec3b>(i, j)[k] / 255;
+
+			}
+		}
+	}
+	float tb = (sum / img.rows * img.cols) - pow((sum2 / img.rows * img.cols), 2);
+	cout << "\nTuong phan= " << tb;
+}
+
+vector <int>cal_histogram(Mat img) {			//Hàm tính histogarm
+	vector <int> h;
+	for (int i = 0; i <= 255; i++) h.push_back(0);
+
+	for (int i = 0; i < img.rows; i++) {
+		for (int j = 0; j < img.cols; j++) {
+			h[img.at<uchar>(i, j)] ++;
+		}
+	}
+	return h;
+}
+
+vector <int>eq_histogram(vector<int> h, int L) {			//Hàm cân bằng
+	vector <int> eh;
+	int tong = 0;
+	vector<int> s;
+
+	for (int i = 0; i <= 255; i++) tong+=h[i];
+	eh.push_back(h[0]);
+	for (int i = 0; i < 256; i++) {
+		eh.push_back(eh[i - 1] + h[i]);
+		s[i] = (int)eh[i] / tong;
+	}
+	return eh;
+}
+
+int main() {
+	Mat img = imread("D:\\Documents\\XLA\\TH\\VD1\\new.jpg", 1);
+	//imshow("Demo", img);
+	int contrast = 2;
+	int c = 10;
+
+	vector<int> h = cal_histogram(img);
+
+	Mat his(300, 300, CV_8UC3, Scalar(0, 0, 0));
+	for (int i = 0; i < 256; i++) {
+		line(his, Point(i, 0), Point(i, 300 - h[i] * 0.1), Scalar(255, 255, 255), 1, 8, 0);
+	}
+	imshow("Histogram", his);
+	
+	//dosang(img);
+	//dotuongphan(img);
+	//tangsang(img, c);
+	//tuongphan(img, contrast,c);
+	//dosang(img);
+	//dotuongphan(img);
+
+
+
+	//tuongphan(img, contrast, c);
+
+	//am ban
+
+
+	/*
+		imshow("red", kenh[2])
+		imshow("green", kenh[0])
+		imshow("blue", kenh[0])
+
+	*/
+	waitKey();
+	return 0;
+}
